7f66c4b16faf7d1939554ab60d74d8809688b337
Lucas Della Bella <dellabella.lucas@gmail.com>
2020-03-08 19:06:36 -0700

    DragnReleaseDivs is broken
    There's a bug with the swapping and I couldn't get the springs to apply... I think there's just a lot going on conceptually and it's confusing me.

--- src/DragnReleaseDivs.js
+++ src/DragnReleaseDivs.js
@@ -5,6 +5,7 @@ import swap from "lodash-move";
 import "./styles.css";
 
 const Div = animated.div;
+const ELEMENT_HEIGHT = 60;
 
 export const MovingDiv = ({ style, children, ...gestureHandlers }) => {
   return (
@@ -14,35 +15,34 @@ export const MovingDiv = ({ style, children, ...gestureHandlers }) => {
   );
 };
 
-const MIN_DRAG_AMT = 60;
-
 export default function App() {
   const [words, setWords] = useState(["A", "B", "C", "D"]);
+  const springs = useSprings(
+    words.length,
+    words.map((w, i) => ({
+      transform: `translate(0px,${i * ELEMENT_HEIGHT}px)`
+    }))
+  );
   // The callback keeps track of how much the user has dragged,
   // .. and when the user has dragged enough, we change its place in our words statelist
   const createHandlers = useDrag(({ movement, last, args }) => {
     const [_, dY] = movement;
     const [currentWord] = args;
     const currentWordIndex = words.indexOf(currentWord);
-    const newSlotOffset = Math.floor(dY / MIN_DRAG_AMT);
+    const newSlotOffset = Math.floor(dY / ELEMENT_HEIGHT);
 
     if (newSlotOffset > -1 && newSlotOffset < words.length) {
       setWords(swap(words, currentWordIndex, newSlotOffset));
     }
-
-    // if (currentWordIndex != newSlotOffset) {
-    //   console.log(currentWordIndex);
-    //   console.log(newSlotOffset);
-    //   console.log("-");
-    // }
   });
 
   return (
     <div className="App">
-      {words.map((currentWord, currentWordIndex) => (
+      {words.map((currentWord, i) => (
         <MovingDiv
           {...createHandlers(currentWord)}
-          style={{ transform: `translate(0px,${currentWordIndex * 60}px)` }}
+          style={{ ...springs[i] }}
+          key={i}
         >
           {currentWord}
         </MovingDiv>
